64a22c1a31cc230c4cca91cbd47be09c431df000
Jack Boberg <jack.boberg@gmail.com>
2011-10-26 14:12:42 -0400

    extend autoloader to third_party models

--- libraries/PHPActiveRecord.php
+++ libraries/PHPActiveRecord.php
@@ -56,8 +56,46 @@ class PHPActiveRecord {
             });
 
         }
+
+        // extend activerecord_autoload() to allow models in /third_party/models/
+        spl_autoload_register('extended_activerecord_autoload',false,PHP_ACTIVERECORD_AUTOLOAD_PREPEND);
     }
 }
+
+// --------------------------------------------------------------------
+
+function extended_activerecord_autoload($class_name)
+{
+    $package_dir = APPPATH.'third_party/';
+    $files = scandir($package_dir);
+    foreach ($files as $file)
+    {
+        if ($file == '.' OR $file == '..')
+            continue;
+        if (is_dir($package_dir.$file) && is_dir($package_dir.$file.'/models/'))
+            $paths[] = $package_dir.$file.'/models/';
+    }
+    foreach ($paths as $path)
+    {
+        $root = realpath($path);
+
+        if (($namespaces = ActiveRecord\get_namespaces($class_name)))
+        {
+            $class_name = array_pop($namespaces);
+            $directories = array();
+
+            foreach ($namespaces as $directory)
+                $directories[] = $directory;
+
+            $root .= DIRECTORY_SEPARATOR . implode($directories, DIRECTORY_SEPARATOR);
+        }
+
+        $file = "$root/$class_name.php";
+    }
+    if (file_exists($file))
+        require $file;
+}
+
 
 /* End of file PHPActiveRecord.php */
 /* Location: ./sparks/php-activerecord/<version>/libraries/PHPActiveRecord.php */
